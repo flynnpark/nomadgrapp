44c5d62c78a894ba4284a518ec07ada106e1f025
Flynn <wphestiraid@gmail.com>
2018-02-09 20:47:20 +0900

    Persisting the Redux Store part Two
    - 디스크에서 스토어를 가져오도록 설정

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from "react";
-import {AppLoading, Asset, Font} from "expo";
-import {Ionicons, MaterialIcons} from "@expo/vector-icons";
-import {StyleSheet, Text, View} from "react-native";
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import configureStore from "./redux/configureStore";
+
+const { persistor, store } = configureStore();
 
 class App extends React.Component {
     state = {
