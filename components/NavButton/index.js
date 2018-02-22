0778877bfa895522cef12cba5d8e96669b546537
Flynn <wphestiraid@gmail.com>
2018-02-22 20:57:21 +0900

    Personalizing the Back Button
    - 뒤로가기 버튼을 만들어서 기존의 버튼을 대체함

---
+++ components/NavButton/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const NavButton = props => (
+    <TouchableWithoutFeedback onPress={props.onPress}>
+        <View style={styles.container}>
+            <Ionicons name={props.iconName} color={props.color} size={30} />
+        </View>
+    </TouchableWithoutFeedback>
+);
+
+NavButton.propTypes = {
+    onPress: PropTypes.func.isRequired,
+    iconName: PropTypes.string.isRequired
+};
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 10
+    }
+});
+
+export default NavButton;
