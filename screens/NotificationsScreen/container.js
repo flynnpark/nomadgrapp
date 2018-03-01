c6f1e2e3e22caa824988363b9235da402e5ba0ef
Flynn <wphestiraid@gmail.com>
2018-03-01 22:10:49 +0900

    NotificationsScreen part One
    - NotificationScreen은 FeedScreen과 거의 동일한 로직
    - FeedScreen과 SearchScreen의 소스를 응용하여 NotificationsScreen 생성
    - Notification 컴포넌트 생성
    - Notification 컴포넌트는 이후 Photo 컴포넌트와 같은 로직으로 클래스 컴포넌트로 생성함

---
+++ screens/NotificationsScreen/container.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Image } from 'react-native';
+import NavButton from '../../components/NavButton';
+import NotificationsScreen from './presenter';
+
+class Container extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        headerTitle: (
+            <Image
+                source={require('../../assets/images/logo.png')}
+                style={{ height: 35 }}
+                resizeMode={'contain'}
+            />
+        ),
+        headerLeft: (
+            <NavButton
+                iconName={'ios-camera-outline'}
+                onPress={() => navigation.navigate('TakePhoto')}
+            />
+        )
+    });
+
+    static propTypes = {
+        notifications: PropTypes.array,
+        getNotifications: PropTypes.func.isRequired
+    };
+
+    state = {
+        isFetching: false
+    };
+
+    componentWillReceiveProps = nextProps => {
+        if (nextProps.notifications) {
+            this.setState({
+                isFetching: false
+            });
+        }
+    };
+
+    _refresh = () => {
+        const { getNotifications } = this.props;
+        this.setState({
+            isFetching: true
+        });
+        getNotifications();
+    };
+
+    render() {
+        return (
+            <NotificationsScreen
+                {...this.props}
+                {...this.state}
+                refresh={this._refresh}
+            />
+        );
+    }
+}
+
+export default Container;
