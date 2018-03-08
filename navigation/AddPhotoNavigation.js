a26dca72f64de9176a7a45bbd3ddc09530f47181
Flynn <wphestiraid@gmail.com>
2018-03-08 21:21:27 +0900

    Uploading a Photo part One
    - CameraScreen 및 LibraryScreen 생성
    - AddPhotoNavigation을 생성하고 Tab으로 CameraScreen과 LibraryScreen을 추가
    - TakePhotoScreen에 AddPhotoNavigation 추가
    - AddPhotoNavigation 스타일링

---
+++ navigation/AddPhotoNavigation.js
@@ -0,0 +1,42 @@
+import { TabNavigator } from 'react-navigation';
+import CameraScreen from '../screens/CameraScreen';
+import LibraryScreen from '../screens/LibraryScreen';
+
+const AddPhotoNavigation = TabNavigator(
+    {
+        Camera: {
+            screen: CameraScreen,
+            navigationOptions: {
+                tabBarLabel: 'Photo'
+            }
+        },
+        Library: {
+            screen: LibraryScreen,
+            navigationOptions: {
+                tabBarLabel: 'Library'
+            }
+        }
+    },
+    {
+        tabBarPosition: 'top',
+        swipeEnabled: true,
+        animationEnabled: true,
+        tabBarOptions: {
+            showLabel: true,
+            upperCaseLabel: true,
+            activeTintColor: '#000',
+            inactiveTintColor: '#bbb',
+            style: {
+                backgroundColor: '#fff',
+                alignItems: 'center'
+            },
+            labelStyle: {
+                fontSize: 16,
+                fontWeight: '600'
+            },
+            showIcon: false
+        }
+    }
+);
+
+export default AddPhotoNavigation;
