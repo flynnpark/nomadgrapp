57b37e099e85f381629d76bbe980db24a4298471
Flynn <wphestiraid@gmail.com>
2018-03-14 20:55:28 +0900

    Uploading a Photo part Nine(Redux Action)
    - uuid를 이용하여 파일 이름이 겹치지 않도록 함
    - photo 액션에서 multipart 전송 구현

--- screens/UploadPhotoScreen/index.js
+++ screens/UploadPhotoScreen/index.js
@@ -1,4 +1,15 @@
 import { connect } from 'react-redux';
 import Container from './container';
+import { actionCreators as photoActions } from '../../redux/modules/photo';
 
-export default connect()(Container);
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        submit: (file, caption, location, tags) => {
+            return dispatch(
+                photoActions.uploadPhoto(file, caption, location, tags)
+            );
+        }
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Container);
